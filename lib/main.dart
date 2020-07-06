29bba001eeffde17dd6003ec8c8cd8f2b8ad3379
Deepanshu Rohilla <deepanshu38841@gmail.com>
2020-07-06 21:17:29 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    ));
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: GestureDetector(),
+      ),
+    );
+  }
+}
+
+class Signature extends CustomPainter {
+  List<Offset> points;
+  Signature({this.points});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = new Paint()
+      ..color = Colors.black
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 5.0;
+
+    for (int i = 0; i < points.length - 1; i++) {
+      if (points[i] != null && points[i + 1] != null) {
+        canvas.drawLine(points[i], points[i + 1], paint);
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(Signature oldDelegate) => oldDelegate.points != points;
+}
